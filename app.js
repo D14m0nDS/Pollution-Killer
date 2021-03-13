6e2b1be8dc7c394288003735e490266114131406
D14m0nDS <80215134+D14m0nDS@users.noreply.github.com>
2021-03-13 14:17:28 +0200

    Revert "ss"
    This reverts commit 14a501216dfeb442f4f91ad8ac6a59183fee6106, reversing
    changes made to 3b3d3e3c84a8c678c7ff69fc4f88425768c77356.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var path = require('path');
@@ -7,33 +8,8 @@ var app = express();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, '/public')));
-
 app.get('/', function (req, res) {
-    var posts = [
-        {
-          author: 'someone',
-          text: 'This is post 1'
-        },
-        {
-          author: 'someone else',
-          text: 'This is post 2'
-        }
-      ];
-
-    res.render('home', {
-      posts: posts
-    });
+    res.render('home', {test:123});
 });
-
-app.get('/sign-in', function (req, res) {
-  res.render('sign-in', {test:123});
-});
-
-app.get('/publish', function (req, res) {
-  res.render('publish', {test:123});
-});
-
 
 app.listen(3000);
-
-console.log('http://localhost:3000')
